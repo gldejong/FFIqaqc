31ce006f101e8f33b7f96cf3607f274226ca2fd7
Eva Deegan <eva_deegan@partner.nps.gov>
2024-04-15 11:07:09 -0700

    started writing new functions and edited some flags apparently, documentation is finalized

--- R/historicalvsyearly.R
+++ R/historicalvsyearly.R
@@ -9,9 +9,9 @@ rm(list = ls())
 
 
 #reading in historical tracker sheet - 2023 for example
-sheet_names_h <- excel_sheets("C:/Users/edeegan/OneDrive - DOI/FFIqaqc/PSME flags_wStatus 2024-02-12 .xlsx")
+sheet_names_h <- excel_sheets("C:/Users/edeegan/OneDrive - DOI/FFIqaqc/PSME flags_newissues 2024-02-12 .xlsx")
 historical <- lapply(sheet_names_h, function(x) {          # Read all sheets to list
-  as.data.frame(read_excel("C:/Users/edeegan/OneDrive - DOI/FFIqaqc/PSME flags_wStatus 2024-02-12 .xlsx", sheet = x)) } )
+  as.data.frame(read_excel("C:/Users/edeegan/OneDrive - DOI/FFIqaqc/PSME flags_newissues 2024-02-12 .xlsx", sheet = x)) } )
 
 names(historical) <- sheet_names_h
 
@@ -29,28 +29,34 @@ for(i in 1:length(yearly)){
 
   h_df=historical[[i]]
   y_df=yearly[[i]]
-  colnames(y_df)=c("Issue","Resolved","Resolved_by","Action_need","Other_notes", "Status")
+  colnames(y_df)[1:5]=c("Issue","Resolved","Resolved_by","Action_need","Other_notes")
   colnames(h_df)=c("Issue","Resolved","Resolved_by","Action_need","Other_notes")
 
   y_df[which(!is.na(y_df$Issue) & y_df$Resolved=="No" | y_df$Resolved=="no"),"Status"]="Previously flagged and unresolved"
   y_df[which(!is.na(y_df$Issue) & y_df$Resolved=="Yes" | y_df$Resolved=="yes"),"Status"]="Previously flagged and resolved"
   setaside_y=y_df[which(y_df$Status=="Previously flagged and unresolved" | y_df$Status=="Previously flagged and resolved"),]
-y_df=y_df[-which(y_df$Status=="Previously flagged and unresolved" | y_df$Status=="Previously flagged and resolved"),]
 
-
-
-
+  if(nrow(setaside_y)>0){
+    y_df=y_df[-which(y_df$Status=="Previously flagged and unresolved" | y_df$Status=="Previously flagged and resolved"),]
+}
   y_df=y_df[,1:5]
   colnames(y_df)=c("Issue","Resolved","Resolved_by","Action_need","Other_notes")
   h_df=h_df[,1:5]
   new=stringdist_right_join(h_df, y_df, by="Issue", method='dl', max_dist=5)
 
   new=new[!duplicated(new$Issue.y),]
+  if(nrow(new)>0){
+
   new[which(is.na(new$Issue.x)),"Status"]="New"
   new[which(!is.na(new$Issue.x) & new$Resolved.x=="No" | new$Resolved.x=="no"),"Status"]="Previously flagged and unresolved"
   new[which(!is.na(new$Issue.x) & new$Resolved.x=="No" | new$Resolved.x=="no"),"Resolved.y"]="No"
   new[which(!is.na(new$Issue.x) & new$Resolved.x=="Yes" | new$Resolved.x=="yes"),"Status"]="Previously flagged and resolved"
   new[which(!is.na(new$Issue.x) & new$Resolved.x=="Yes" | new$Resolved.x=="yes"),"Resolved.y"]="Yes"
+  new[which(is.na(new$Status)), "Status"]="New"
+  }else{
+  new[1,]=NA
+  new$Status=NA
+}
   y_df=new[,-which(grepl("x", colnames(new)))]
   colnames(y_df)=c("Issue","Resolved","Resolved_by","Action_need","Other_notes", "Status")
   y_df=rbind(y_df, setaside_y)
---
+++ newfunctions.R
@@ -0,0 +1,54 @@
+setwd("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/")
+rm(list = ls())
+mtype="PSME"
+source("C:/Users/edeegan/OneDrive - DOI/FFIqaqc/R/datacleaningfunctions.R")
+samp=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/SAGU_SampleEventReport.csv")
+cover=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/PSME_Cover - Species Composition (metric)_XPT.csv", na.strings=c("","NA"))
+fuel1000=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/PSME_Surface Fuels - 1000Hr_XPT.csv", na.strings=c("","NA"))
+duff=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/PSME_Surface Fuels - Duff_Litter_XPT.csv", na.strings=c("","NA"))
+fine=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/PSME_Surface Fuels - Fine_XPT.csv", na.strings=c("","NA"))
+saps=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/PSME_Trees - Saplings (Diameter Class) (metric)_XPT.csv", na.strings=c("","NA"))
+seeds=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/PSME_Trees - Seedlings (Height Class) (metric)_XPT.csv")
+tree=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/PSME_Trees - Individuals (metric)_XPT.csv")
+pbsev=read.csv("C:/Users/edeegan/OneDrive - DOI/Fire_project/Fire_project/SAGU_data/PSME/PSME_Post Burn Severity (metric)_XPT.csv")
+
+
+flags=c()
+#new functions
+
+#missing dbh
+
+#every tree should have a dbh that isn't dead and down
+tree=tree[-which(tree$CrwnCl=="DD" | tree$CrwnCl=="BBD"),]
+tree=tree[-which(tree$Species.Symbol=="CANOPY"),]
+no_dbh=tree[which(is.na(tree$DBH)),]
+#also status of X - didn't find or measure
+
+unique(no_dbh$Species.Symbol)
+
+#species change - CHANGE TO PER PLOT
+tags=unique(tree$TagNo)
+for(x in 1:length(tags)){
+  tree_i=tree[which(tree$TagNo==tags[x]),]
+  if(length(unique(tree_i$Species.Symbol))==1){
+    #all good
+  }else{
+    #flag
+    flags<-c(flags, paste(c("Tag number ", tags[x],
+                          " in tree data set switches species:", paste(tree_i$Species.Symbol, sep=", "),
+                          "recorded for species in sample events",
+                          paste(tree_i[which(tree_i$TagNo==tags[x]),"MacroPlot.Name"],
+                          tree_i[which(tree_i$TagNo==tags[x]),"Monitoring.Status"], collapse=", ")), collapse=" "))
+  }
+}
+
+
+
+#correct subfraction
+poles=tree[which(tree$DBH<15.1),]
+overstory=tree[which(tree$DBH>=15.1),]
+
+poles[which(poles$SubFrac!=0.25),]
+overstory[which(overstory$SubFrac!=1),]
+#shrub functions
+
